34580a82226ef8b38d68112a2ead0ebdf854999c
unknown <kumar.ankit383@gmail.com>
2020-12-20 00:52:49 +0530

    Array program

---
+++ Day 11 to 20/Arrays/ArrayExample.java
@@ -0,0 +1,43 @@
+class MyArray {
+    
+    int[] arr;
+    int size;
+
+    public MyArray () {
+        this.arr = new int[5];
+        this.size = 5;
+    }
+
+    public MyArray(int size) {
+        this.arr = new int[size];
+        this.size = size;
+    }
+
+    public void insert(int sk) 
+    {
+
+    }
+
+    public int delete(int sk) 
+    {
+        return -1;
+    }
+
+    public int search(int sk)
+    {
+        return -1;
+    }
+
+    public int iterativeBinarySearch(int sk)
+    {
+        return -1;
+    }
+
+}
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        
+    }
+}
